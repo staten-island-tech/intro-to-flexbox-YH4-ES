5683ac24ad285a74f16c08f7c95c565513560513
YH4-ES <yifanh4@nycstudents.net>
2025-10-08 00:01:44 -0400

    coolbeans

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ function inject() { //Trying to get this button to inject. Reminder to delete ol
   DOMSelectors.fictional.forEach(item => { //to inject
     DOMSelectors.container.insertAdjacentHTML("afterbegin", item. outerHTML);
   });
+  document.querySelector(".toShowAndToKnow").textContent = "Now Showing:";
   document.querySelector(".thecurrentShow").textContent = "Welcome to the Fiction.";
   document.getElementById("plentyOfExuberance").volume = 0.3;
   document.getElementById("plentyOfExuberance").play();
-  document.querySelector(".playItOnAndNeverStop").src = "recordingme.png"
+  document.querySelector(".playItOnAndNeverStop").src = "recordingme.png";
+  document.getElementById("fictionbutton").style.color="pink";
 
 }
 document.getElementById("fictionbutton").addEventListener("click", inject);
